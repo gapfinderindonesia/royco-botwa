8fd28f1f5ae667ffd5b5b4c2daa2a5bf6f643cb7
gapfinderindonesia <75089710+gapfinderindonesia@users.noreply.github.com>
2021-02-03 23:50:09 +0800

    Update msgHndlr.js

--- msgHndlr.js
+++ msgHndlr.js
@@ -1463,7 +1463,7 @@ module.exports = msgHandler = async (client, message) => {
             const chatz = await client.getAllChatIds()
             for (let ids of chatz) {
                 var cvk = await client.getChatById(ids)
-                if (!cvk.isReadOnly) await client.sendText(ids, `[ CR4R BOT Broadcast ]\n\n${msg}`)
+                if (!cvk.isReadOnly) await client.sendText(ids, `[ GFI BOT Broadcast ]\n\n${msg}`)
             }
             client.reply(from, 'Broadcast Success!', id)
             break
